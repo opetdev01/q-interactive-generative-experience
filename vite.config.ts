f84ba791d14fdf27df6a197af1636cf055e60f81
unknown <pc@cube101.Cube.local>
2026-07-29 10:07:37 +0300

    Include all media assets, logos, and videos in production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    copyPublicDir: false
-  },
   server: {
     host: true
   }
